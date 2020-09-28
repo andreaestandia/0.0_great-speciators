12632abeca0943f55d5a1e1f6868e98580e39a34
andreaestandia <andrea.estandia@env-res.ox.ac.uk>
2020-09-28 09:20:55 +0100

    updated again the R test script to relative paths and complete path for source only

--- notebooks/cluster_scripts/calculate_area_dist.R
+++ notebooks/cluster_scripts/calculate_area_dist.R
@@ -5,14 +5,15 @@
 knitr::opts_knit$set(root.dir = rprojroot::find_rstudio_root_file())
 knitr::opts_chunk$set(echo = TRUE, eval = FALSE)
 options(scipen = 999)
-source("~/data/zool-zost/sjoh4959/projects/0.0_great-speciators/src/0.0_great-speciators_source.R")
+source("/data/zool-zost/sjoh4959/projects/0.0_great-speciators/src/0.0_great-speciators_source.R")
 
 #############################################
 ########## LOAD INTERSECTION DATA ###########
 #############################################
 
-load("~/data/zool-zost/sjoh4959/projects/0.0_great-speciators/data/intersection2.RData")
-load("~/data/zool-zost/sjoh4959/projects/0.0_great-speciators/data/intersection.RData")
+load(file.path(data_path,"intersection.RData"))
+load(file.path(data_path,"intersection2.RData"))
+
 
 #############################################
 ########## CALCULATE INTERSECTION DATA ######
@@ -23,4 +24,4 @@ for (i in list(c(sp0intersection, sp1intersection, sp1.1intersection, sp1.2inter
   resident_distr <- rbind(calculateDistribution(i))
 }
 
-save(resident_distr, file="~/data/zool-zost/sjoh4959/projects/0.0_great-speciators/data/distr_index.RData")
+save(resident_distr, file=file.path(data_path,"distr_index.RData"))
