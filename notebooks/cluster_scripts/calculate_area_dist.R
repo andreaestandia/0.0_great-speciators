493f9076a2380f1c8699026a3234d5cef7e7dc55
andreaestandia <andrea.estandia@env-res.ox.ac.uk>
2020-09-28 08:44:24 +0100

    Updated the first R script to submit as a job in the cluster

--- notebooks/cluster_scripts/calculate_area_dist.R
+++ notebooks/cluster_scripts/calculate_area_dist.R
@@ -5,7 +5,7 @@
 knitr::opts_knit$set(root.dir = rprojroot::find_rstudio_root_file())
 knitr::opts_chunk$set(echo = TRUE, eval = FALSE)
 options(scipen = 999)
-source("./src/0.0_great-speciators_source.R")
+source("~/data/zool-zost/sjoh4959/projects/0.0_great-speciators/src/0.0_great-speciators_source.R")
 
 #############################################
 ########## LOAD INTERSECTION DATA ###########
@@ -15,15 +15,15 @@ load("~/data/zool-zost/sjoh4959/projects/0.0_great-speciators/data/intersection2
 sp0 <- sf::st_read(dsn = "~/data/zool-zost/sjoh4959/projects/0.0_great-speciators/data/All_Species_Resident_1.2.shp")
 land <- sf::st_read(dsn = "~/data/zool-zost/sjoh4959/projects/0.0_great-speciators/data/everythingisanisland.shp")
 
-sp0_intersection <- calculateArea(sp0, land)
+sp0intersection <- calculateArea(sp0, land)
 
 #############################################
-########## CALCULATE INTERSECTION DATA ###########
+########## CALCULATE INTERSECTION DATA ######
 #############################################
 
 resident_distr <- data.frame()
-for (i in list(c(sp1intersection, sp1.1intersection, sp1.2intersection, sp1.3intersection, sp1.4intersection))) {
+for (i in list(c(sp0intersection, sp1intersection, sp1.1intersection, sp1.2intersection, sp1.3intersection, sp1.4intersection))) {
   resident_distr <- rbind(calculateDistribution(i))
 }
 
-
+save(resident_distr, file="distr_index.RData")
