089528e4442c2dfdb7427cbaf0260d1cd9a1af63
andreaestandia <andrea.estandia@env-res.ox.ac.uk>
2020-09-28 08:33:35 +0100

    Created my first script to upload an R to the cluster

---
+++ notebooks/cluster_scripts/calculate_area_dist.R
@@ -0,0 +1,29 @@
+#############################################
+############# LOAD SOURCE CODE ##############
+#############################################
+
+knitr::opts_knit$set(root.dir = rprojroot::find_rstudio_root_file())
+knitr::opts_chunk$set(echo = TRUE, eval = FALSE)
+options(scipen = 999)
+source("./src/0.0_great-speciators_source.R")
+
+#############################################
+########## LOAD INTERSECTION DATA ###########
+#############################################
+
+load("~/data/zool-zost/sjoh4959/projects/0.0_great-speciators/data/intersection2.RData")
+sp0 <- sf::st_read(dsn = "~/data/zool-zost/sjoh4959/projects/0.0_great-speciators/data/All_Species_Resident_1.2.shp")
+land <- sf::st_read(dsn = "~/data/zool-zost/sjoh4959/projects/0.0_great-speciators/data/everythingisanisland.shp")
+
+sp0_intersection <- calculateArea(sp0, land)
+
+#############################################
+########## CALCULATE INTERSECTION DATA ###########
+#############################################
+
+resident_distr <- data.frame()
+for (i in list(c(sp1intersection, sp1.1intersection, sp1.2intersection, sp1.3intersection, sp1.4intersection))) {
+  resident_distr <- rbind(calculateDistribution(i))
+}
+
+
